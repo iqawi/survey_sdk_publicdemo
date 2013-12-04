52700a63f7eca6e68410d8dff617affe6ffb791b
James Woodroffe <jim@woodroffe.com>
2013-12-04 09:00:33 -0800

    Applying automated formatting rules

--- Survey SDK Demo/survey_sdk_demoViewController.m
+++ Survey SDK Demo/survey_sdk_demoViewController.m
@@ -12,48 +12,51 @@
 
 @synthesize lblAvailable;
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    // SDK call to initialize the user
-    // Can be called as soon as the user information is available or as late as immediately before the Show methods
-    // If an email is not available, please use a distinct id of some kind
-    [Survey_SDK setupUser:@"test@test123.com"];
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+
+	// SDK call to initialize the user
+	// Can be called as soon as the user information is available or as late as immediately before the Show methods
+	// If an email is not available, please use a distinct id of some kind
+	[Survey_SDK setupUser:@"test@test123.com"];
 }
 
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-    
-    // Call this function to get a count of available surveys for the user (OPTIONAL)
-    self.lblAvailable.text = @"Surveys Available: (loading...)";
-    [Survey_SDK requestSurveyCount:(id)self];
+- (void)viewDidAppear:(BOOL)animated
+{
+	[super viewDidAppear:animated];
+
+	// Call this function to get a count of available surveys for the user (OPTIONAL)
+	self.lblAvailable.text = @"Surveys Available: (loading...)";
+	[Survey_SDK requestSurveyCount:(id)self];
 }
 
 // Implement this delegate function to respond to the survey count request (OPTIONAL)
-- (void)surveyCount:(NSUInteger)count {
-    NSLog(@"Survey Available: %lu", (unsigned long)count);
-    self.lblAvailable.text = [NSString stringWithFormat:@"Surveys Available: %lu", (unsigned long)count];
+- (void)surveyCount:(NSUInteger)count
+{
+	NSLog(@"Survey Available: %lu", (unsigned long)count);
+	self.lblAvailable.text = [NSString stringWithFormat:@"Surveys Available: %lu", (unsigned long)count];
 }
 
-- (IBAction)btnAll_click:(id)sender {
-    
-    // SDK call to load the list of available surveys
-    [Survey_SDK showSurveyList];
+- (IBAction)btnAll_click:(id)sender
+{
+	// SDK call to load the list of available surveys
+	[Survey_SDK showSurveyList];
 }
 
-- (IBAction)btnOne_click:(id)sender {
-    
-    // SDK call to load a single survey
-    // Can be called with a surveyid to load a specfic survey or without an id to load the "next" survey for a user
-    // returnToList - YES means that after a complete the list of available surveys will appear
-    [Survey_SDK showSurvey:@"51068260ad3af20002000136" returnToList:NO];
+- (IBAction)btnOne_click:(id)sender
+{
+	// SDK call to load a single survey
+	// Can be called with a surveyid to load a specfic survey or without an id to load the "next" survey for a user
+	// returnToList - YES means that after a complete the list of available surveys will appear
+	[Survey_SDK showSurvey:@"51068260ad3af20002000136" returnToList:NO];
 }
 
-- (IBAction)btnLogout:(id)sender {
-    
-    // SDK call to logout the user
-    // This should be called when a user logs out of the host app
-    [Survey_SDK logoutUser];
+- (IBAction)btnLogout:(id)sender
+{
+	// SDK call to logout the user
+	// This should be called when a user logs out of the host app
+	[Survey_SDK logoutUser];
 }
 
 @end
